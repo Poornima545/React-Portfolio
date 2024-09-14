3c47606fcc2debea5f30444b8e06639976bdcb40
Poornima545 <poornimac714@gmail.com>
2024-09-14 21:37:23 +0530

    fixing netlify issue

--- src/components/Projects/FirstProject.js
+++ src/components/Projects/FirstProject.js
@@ -7,8 +7,8 @@ export default function FirstProject() {
             <div className="card">
                 <img src={FirstImage} className="card-img-top" alt="First Image" />
                 <div className="card-body p-4">
-                    <h5 className="card-title">First Project</h5>
-                    <p className="card-text lh-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.</p>
+                    <h5 className="card-title">Slider Project</h5>
+                    <p className="card-text lh-lg mt-3">The Slider Project is a React application that showcases a carousel of team member profiles. Each slide includes an image, name, job title, and a brief description, with customized navigation icons for a smooth user experience.</p>
                     <div className='d-flex align-items-center'>
                         <a href="https://www.example.com"
                             target="_blank"
@@ -16,7 +16,7 @@ export default function FirstProject() {
                             className='me-2 icon'>
                             <AiOutlineGlobal size={30} />
                         </a>
-                        <a href="https://github.com/Poornima545"
+                        <a href="https://github.com/Poornima545/react-projects/pull/1"
                             target="_blank"
                             rel="noopener noreferrer"
                             className='me-2 icon'>

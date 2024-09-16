40e1e49f819189957a7c3d9f8ce04f5072ebc6ef
Poornima545 <poornimac714@gmail.com>
2024-09-16 13:52:13 +0530

    updated in firstproject file

--- src/components/Projects/FirstProject.js
+++ src/components/Projects/FirstProject.js
@@ -11,17 +11,17 @@ export default function FirstProject() {
                     <h5 className="card-title">Slider Project</h5>
                     <p className="card-text lh-lg mt-3">The Slider Project is a React application that showcases a carousel of team member profiles. Each slide includes an image, name, job title, and a brief description, with customized navigation icons for a smooth user experience.</p>
                     <div className='d-flex align-items-center'>
-                        <a href="https://www.example.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className='me-2 icon'>
-                            <AiOutlineGlobal size={30} />
-                        </a>
-                        <a href="https://github.com/Poornima545/react-projects/pull/1"
+                        <a href="https://github.com/Poornima545/react-slider/pull/1"
                             target="_blank"
                             rel="noopener noreferrer"
                             className='me-2 icon'>
                             <AiFillGithub size={30} />
+                        </a>
+                        <a href="https://myslider-project.netlify.app/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='me-2 icon'>
+                            <AiOutlineGlobal size={30} />
                         </a>
                     </div>
                 </div>
